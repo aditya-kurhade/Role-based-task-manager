727f6ae81cbdfd56cc35f0fbe3296f1d7c33f922
Aditya Kurhade <adityakurhade1000@gmail.com>
2026-05-07 23:37:39 +0530

    Remove hardcoded DATABASE_URL for improved security and flexibility

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://neondb_owner:npg_vwV9c3YFZHCz@ep-late-wildflower-ambz94tj-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+DATABASE_URL = ""
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
